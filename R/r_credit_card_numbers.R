7ede2396b4f720758cf25eb04a0f9be4ec89faa4
paulhendricks <paul.hendricks.2013@owu.edu>
2015-08-25 21:18:39 +0000

    update with unit tests

--- R/r_credit_card_numbers.R
+++ R/r_credit_card_numbers.R
@@ -8,5 +8,4 @@ r_credit_card_numbers <- function(n) {
                sample(1000:9999, size = n, replace = TRUE),
                sample(1000:9999, size = n, replace = TRUE),
                sample(1000:9999, size = n, replace = TRUE), sep = "-"))
-  return("To be updated: depends on Card Type.")
   }
--- tests/testthat/test-r_phone_numbers.R
+++ tests/testthat/test-r_phone_numbers.R
@@ -3,6 +3,7 @@ context("r_phone_numbers()")
 
 test_that("Produces the correct output.", {
   expect_equal(length(r_phone_numbers(100)), 100)
+  expect_equal(length(r_phone_numbers(100, use_parentheses = TRUE)), 100)
 })
 
 test_that("Produces the correct output type.", {
